ed522981fe01ed7caf338372cc728a61f449aaaf
Ben-Blance <sahilandhare2004@gmail.com>
2024-10-10 00:39:18 +0530

    things

--- super/share.js
+++ super/share.js
@@ -1,4 +1,4 @@
-// ... (previous code remains the same)
+
 
 // Share functionality
 function shareOnTwitter() {
@@ -15,8 +15,7 @@ function shareOnFacebook() {
 function shareOnPinterest() {
     const url = encodeURIComponent(window.location.href);
     const description = encodeURIComponent("Super Tic-Tac-Toe Game");
-    // Note: For Pinterest, you should ideally have an image to share. 
-    // Replace 'IMAGE_URL' with an actual image URL from your game.
+ 
     const imageUrl = encodeURIComponent('super\\photos\\image.jpg');
     window.open(`https://pinterest.com/pin/create/button/?url=${url}&media=${imageUrl}&description=${description}`, '_blank');
 }
@@ -25,13 +24,11 @@ function shareOnTumblr() {
     const url = encodeURIComponent(window.location.href);
     const title = encodeURIComponent("Super Tic-Tac-Toe");
     const caption = encodeURIComponent("I'm playing Super Tic-Tac-Toe! Check it out!");
-    window.open(`https://www.tumblr.com/widgets/share/tool?canonicalUrl=${url}&title=${title}&caption=${caption}`, '_blank');
+    window.open(`https://github.com/ben-blance/Super-Tic-Tac-Toe`, '_blank');
 }
 
-// Add event listeners to share buttons
 document.getElementById('twitter-share').addEventListener('click', shareOnTwitter);
 document.getElementById('facebook-share').addEventListener('click', shareOnFacebook);
 document.getElementById('pinterest-share').addEventListener('click', shareOnPinterest);
 document.getElementById('tumblr-share').addEventListener('click', shareOnTumblr);
 
-// ... (rest of the previous code remains the same)
